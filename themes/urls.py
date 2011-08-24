2dd4561ae1b26a0ed64b42b2975f23cbd96919ac
Marinho Brandao <marinho@gmail.com>
2011-08-24 07:58:32 -0300

    Static file uploading

--- themes/urls.py
+++ themes/urls.py
@@ -5,5 +5,6 @@ import views
 urlpatterns = patterns('',
     url(r'^$', views.home, name='themes_home'),
     url(r'^([\w\-_]+)/$', views.theme, name='themes_theme'),
+    url(r'^([\w\-_]+)/up-file/$', views.theme_up_file, name='themes_up_file'),
 )
 
--- themes/views.py
+++ themes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
 
 from models import Theme, ThemeTemplate, ThemeStaticFile
 
@@ -16,6 +18,31 @@ def theme(request, name):
     return render_to_response(
             'themes/theme.html',
             {'theme': theme},
+            context_instance=RequestContext(request),
+            )
+
+@csrf_exempt
+def theme_up_file(request, name):
+    theme = get_object_or_404(Theme, name=name)
+    new_static_files = []
+
+    if request.method == 'POST':
+        for up_file in request.FILES.getlist('dragupload[]'):
+            name = up_file.name
+            while theme.static_files.filter(name=name).count():
+                name += '_'
+
+            st = ThemeStaticFile()
+            st.theme = theme
+            st.name = name
+            st.file = up_file
+            st.save()
+
+            new_static_files.append(st)
+
+    return render_to_response(
+            'themes/theme_up_file.html',
+            {'theme': theme, 'new_static_files': new_static_files},
             context_instance=RequestContext(request),
             )
 
